d821040e462f75dd2496eb5a970fcfdf7f1cc904
Siva Prakash <prakashdeveloper@outlook.in>
2024-01-23 18:34:16 +0530

    Added RemoveDuplicates.cpp file

---
+++ LinkedList/Remove Duplicates from Sorted List II/RemoveDuplicates.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class ListNode {
+ public:
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+ public:
+  ListNode* deleteDuplicates(ListNode* head) {
+    ListNode* dummy = new ListNode(-1);
+    dummy->next = head;
+    ListNode* prev = dummy;
+    ListNode* temp = head;
+    while (temp != nullptr) {
+      if (temp->next != nullptr && temp->val == temp->next->val) {
+        while (temp->next != nullptr && temp->val == temp->next->val) {
+          temp = temp->next;
+        }
+        prev->next = temp->next;
+      } else {
+        prev = prev->next;
+      }
+      temp = temp->next;
+    }
+
+    return dummy->next;
+  }
+};
+
+class RemoveDuplicates {
+ public:
+  ListNode* createLinkedList(const vector<int>& values) {
+    ListNode* dummyHead = new ListNode(0);
+    ListNode* current = dummyHead;
+    for (int value : values) {
+      current->next = new ListNode(value);
+      current = current->next;
+    }
+    return dummyHead->next;
+  }
+
+  void printLinkedList(ListNode* node) {
+    while (node != nullptr) {
+      cout << node->val;
+      if (node->next != nullptr) {
+        cout << " -> ";
+      }
+      node = node->next;
+    }
+    cout << endl;
+  }
+};
+
+int main() {
+  int tc;
+  cin >> tc;
+  Solution solution;
+  RemoveDuplicates helper;
+  while (tc-- > 0) {
+    string l1Input;
+    cin >> l1Input;
+    vector<int> l1Values;
+    stringstream ss(l1Input);
+    string token;
+    while (getline(ss, token, ',')) {
+      l1Values.push_back(stoi(token));
+    }
+    ListNode* head = helper.createLinkedList(l1Values);
+    head = solution.deleteDuplicates(head);
+    helper.printLinkedList(head);
+  }
+  return 0;
+}
